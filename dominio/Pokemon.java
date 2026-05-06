fc8ea57c019e4c90b31575da714b5e4e826fa5f5
tomaszepeda2411 <tomas.zepeda01@alumnos.ucn.cl>
2026-05-06 11:03:05 -0400

    leerpokedex

--- dominio/Pokemon.java
+++ dominio/Pokemon.java
@@ -1,5 +1,61 @@
 package dominio;
 
 public class Pokemon {
+	private String nombre;
+	private String habitat;
+	private double porcentajeAparicion;
+	private int vida;
+	private int ataque;
+	private int defensa;
+	private int ataqueEspecial;
+	private int defensaEspecial;
+	private int velocidad;
+	private String tipo;
+	private int totalStats;
+	
+	public Pokemon(String nombre, String habitat, double porcentajeAparicion, int vida, int ataque, int defensa,
+			int ataqueEspecial, int defensaEspecial, int velocidad, String tipo) {
+		
+		this.nombre = nombre;
+		this.habitat = habitat;
+		this.porcentajeAparicion = porcentajeAparicion;
+		this.vida = vida;
+		this.ataque = ataque;
+		this.defensa = defensa;
+		this.ataqueEspecial = ataqueEspecial;
+		this.defensaEspecial = defensaEspecial;
+		this.velocidad = velocidad;
+		this.tipo = tipo;
+		totalStats = calcularStats();
+	}
+	
+	private int calcularStats() {
+		int total = 0;
+		total += (vida + ataque + defensa + ataqueEspecial + defensaEspecial + velocidad);
+		
+		return total;
+	}
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getHabitat() {
+		return habitat;
+	}
+
+	public double getPorcentajeAparicion() {
+		return porcentajeAparicion;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public int getTotalStats() {
+		return totalStats;
+	}
+	
+	
+	
 }
--- logica/App.java
+++ logica/App.java
@@ -1,10 +1,73 @@
 package logica;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import dominio.*;
+
+
 public class App {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static Scanner scan;
+	private static ArrayList<Pokemon> pokemones = new ArrayList<>();
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		leerPokedex();
+		leerGimnasios();
+		leerAltoMando();
+		leerRegistros();
+		leerHabitats();
+		
+		desplegarMenu();
+		
 
+	}
+	private static void desplegarMenu() {
+		// TODO Auto-generated method stub
+		
+	}
+	private static void leerHabitats() {
+		// TODO Auto-generated method stub
+		
+	}
+	private static void leerRegistros() {
+		// TODO Auto-generated method stub
+		
+	}
+	private static void leerAltoMando() {
+		// TODO Auto-generated method stub
+		
+	}
+	private static void leerGimnasios() {
+		// TODO Auto-generated method stub
+		
+	}
+	private static void leerPokedex() throws FileNotFoundException {
+		File txtPokedex = new File("Pokedex.txt");
+		scan = new Scanner(txtPokedex);
+		while (scan.hasNextLine()) {
+		String linea = scan.nextLine();
+		String partes[] = linea.split(";");
+		
+		String nombre = partes[0];
+		String habitat = partes[1];
+		double porcentaje = Double.parseDouble(partes[2]);
+		int vida = Integer.parseInt(partes[3]);
+		int ataque = Integer.parseInt(partes[4]);
+		int defensa = Integer.parseInt(partes[5]);
+		int ataqueEspecial = Integer.parseInt(partes[6]);
+		int defensaEspecial = Integer.parseInt(partes[7]);
+		int velocidad = Integer.parseInt(partes[8]);
+		String tipo = partes[9];
+		
+		Pokemon nuevoPokemon = new Pokemon(nombre,habitat,porcentaje,vida,ataque,defensa,ataqueEspecial,defensaEspecial,velocidad,tipo);
+		
+		pokemones.add(nuevoPokemon);
+		
+		
+		}
+		
 	}
 
 }
