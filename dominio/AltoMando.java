86a5bb4374317e4ee50da22aafb28ca401a72f01
Kevin Zamora Riquelme <kevin.zamora@alumnos.ucn.cl>
2026-05-08 21:20:36 -0400

    Update AltoMando.java

--- dominio/AltoMando.java
+++ dominio/AltoMando.java
@@ -1,22 +1,34 @@
 package dominio;
 
+import java.util.ArrayList;
+
 public class AltoMando {
+	
 	private int numero;
 	private String nombre;
+	private ArrayList<Pokemon> pokemons;
 	
 	
-	
-	public AltoMando(int numero, String nombre) {
-		super();
+	public AltoMando(int numero, String nombre, ArrayList<Pokemon> pokemons) {
+		
 		this.numero = numero;
 		this.nombre = nombre;
+		this.pokemons = pokemons;
 	}
+	
 	public int getNumero() {
 		return numero;
 	}
 	public String getNombre() {
 		return nombre;
 	}
+
+	public ArrayList<Pokemon> getPokemons() {
+		return pokemons;
+	}
+
+	
+	
 	
 	
 }
